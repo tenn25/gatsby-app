d470ca33458b060edf51bbb2e12f6ab8403f7d68
tenn25 <endo_ry@mti.co.jp>
2022-07-07 00:42:23 +0900

    add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Link } from 'gatsby';
+
+const AboutPage = () => {
+  return (
+    <React.Fragment>
+    <div>About page</div>
+    <Link to="/">Back to home</Link>
+  </React.Fragment>
+  )
+}
+
+export default AboutPage
